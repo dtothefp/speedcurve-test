2d7b7561cdde3398c226d22f3d55d0cc9cfe9d5c
dtothefp <dtothefp@gmail.com>
2016-06-03 16:42:42 -0400

    add back global flag for git options

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config user.name "dtothefp"
-git config user.email "dtothefp@gmail.com"
+git config --global user.name "dtothefp"
+git config --global user.email "dtothefp@gmail.com"
 
 git remote rm origin
 git remote add origin https://dtothefp:${GITHUB_API_KEY}@github.com/dtothefp/speedcurve-test.git
