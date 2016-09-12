b59823b6224c93ff9423b89a0d83fcb8e43cc4bb
Travis CI <davidfox-powell@dfp.local>
2016-09-12 14:57:54 -0400

    play with encrypt again

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-
 git config --global user.name "Travis CI"
 git config --global user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -17,5 +14,5 @@ chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
-git remote rm origin
-git remote add origin $SSH_REPO
+# git remote rm origin
+# git remote add origin $SSH_REPO
