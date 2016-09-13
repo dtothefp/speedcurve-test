445252cfe07ed6637c1eb1ade07a3c43e2b719fd
Travis CI <davidfox-powell@dfp.local>
2016-09-12 22:49:47 -0400

    add caching back to the correct place

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ function doCompile {
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
+  echo "Skipping deploy; just doing a build."
+  doCompile
+  exit 0
 fi
 
 # Save some useful information
@@ -34,12 +34,6 @@ rm -rf $BUILD_TARGET/**/* || exit 0
 # Run our compile script
 doCompile
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-# if [ -z `git diff --exit-code` ]; then
-    # echo "No changes to the output on this push; exiting."
-    # exit 0
-# fi
-
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
@@ -54,6 +48,12 @@ ssh-add deploy_key
 cd $BUILD_TARGET
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+  echo "No changes to the output on this push; exiting."
+  exit 0
+fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
